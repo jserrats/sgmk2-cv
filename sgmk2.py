8782b700c5d6cf1f21bb12986b308ad17c2fa23a
jserrats <jaume.serrats9@gmail.com>
2023-12-23 12:43:54 +0100

    mqtt looping on separate thread

--- sgmk2.py
+++ sgmk2.py
@@ -13,24 +13,24 @@ class SGMk2:
         self.laser = False
         self.rof = 0
         self.autoaim = False
-        
+
     def alert(self):
         self.client.publish("sgmk2/alert", "1")
-        self.client.loop()
-        
+
     def get_rof_value(self):
         return self._rof_values[self.rof]
 
     def connect_mqtt(self):
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
-                print("Connected to MQTT Broker!")
-            else:
-                print("Failed to connect, return code %d\n", rc)
+                client.publish("sgmk2-cv/status", "online", retain=True)
 
         client = mqtt_client.Client(client_id)
         client.on_connect = on_connect
+        client.will_set("sgmk2-cv/status", "offline", retain=True)
         client.connect(broker, port)
+        client.loop_start()
+
         return client
 
     def flywheel(self, value):
@@ -39,26 +39,20 @@ class SGMk2:
         else:
             self.client.publish("sgmk2/flywheel", "off")
 
-        self.client.loop()
-
     def shooting(self, value):
         if value:
             self.client.publish("sgmk2/shoot", "on")
         else:
             self.client.publish("sgmk2/shoot", "off")
-        self.client.loop()
 
     def pan_rel(self, value):
         self.client.publish("sgmk2/pan/relative", value)
-        self.client.loop()
 
     def tilt_rel(self, value):
         self.client.publish("sgmk2/tilt/relative", value)
-        self.client.loop()
 
     def _set_rof(self, value):
         self.client.publish("sgmk2/rof", value)
-        self.client.loop()
 
     def switch_rof(self):
         self.rof = (self.rof + 1) % 3
@@ -69,5 +63,4 @@ class SGMk2:
             self.client.publish("sgmk2/laser", "on")
         else:
             self.client.publish("sgmk2/laser", "off")
-        self.client.loop()
         self.laser = not self.laser
